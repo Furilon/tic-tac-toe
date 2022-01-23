b84589152b47d8fe58efb4d20ef2bbd06ec7e55b
Mykyta Medvediev <arlekin8881@gmail.com>
2022-01-22 16:41:49 -0800

    Wrote the logic of the game. At this point in time, displayController module mixes functions of both display control and game flow. It would be nice to separate the two. Otherwise, game works perfectly, even better than the last time I built it since it disables all of the buttons after moves and after the game ends.

---
+++ script.js
@@ -0,0 +1,89 @@
+const gameBoard = (() => {
+    let _board = [,,,,,,,,];
+
+    const makeMove = (sign, position) => {
+        _board[position] = sign;
+    };
+    const clearBoard = () => {
+        _board = [,,,,,,,,]
+    };
+    const checkResult = (sign) => {
+        if ((_board[0] == sign && _board[1] == sign && _board[2] == sign) ||
+        (_board[3] == sign && _board[4] == sign && _board[5] == sign) ||
+        (_board[6] == sign && _board[7] == sign && _board[8] == sign) ||
+        (_board[0] == sign && _board[3] == sign && _board[6] == sign) ||
+        (_board[1] == sign && _board[4] == sign && _board[7] == sign) ||
+        (_board[2] == sign && _board[5] == sign && _board[8] == sign) ||
+        (_board[0] == sign && _board[4] == sign && _board[8] == sign) ||
+        (_board[2] == sign && _board[4] == sign && _board[6] == sign)) {
+            return 'win';
+        } else if (!_board.includes(undefined)) {
+            return 'tie';
+        } else return false;
+    };
+    const printBoard = () => _board;
+
+    return { makeMove, clearBoard, checkResult, printBoard }
+})(); 
+
+const player = (() => {
+    let _sign = 'x';
+    const switchSign = () => {
+        if (_sign == 'x') {
+            _sign = 'o';
+        } else _sign = 'x'
+    };
+    const getSign = () => _sign;
+    const reset = () => {
+        _sign = 'x';
+    }
+
+    return { switchSign, getSign, reset }
+})();
+
+const displayController = (() => {
+    const cells = document.querySelectorAll(".cell");
+    const result = document.querySelector('h2');
+    const move = document.getElementById('div-move');
+    const startOverBtn = document.querySelector('button'); 
+    
+    const _makeCellInactive = (e) => {
+        e.target.style.pointerEvents = "none";
+    }
+
+    const _startOver = () => {
+        cells.forEach(cell => {
+            cell.style.pointerEvents = "";
+            cell.textContent = ""
+        });
+        result.textContent = "";
+
+        gameBoard.clearBoard();
+        player.reset();
+    } 
+
+
+    const _play = (e) => {
+        const position = e.target.id;
+        const sign = player.getSign();
+        gameBoard.makeMove(sign, position);
+        console.log(sign, position, gameBoard.printBoard());
+        e.target.textContent = sign;
+        _makeCellInactive(e);
+        move.textContent = player.getSign();
+        
+        const gameResult = gameBoard.checkResult(sign);        
+        
+        if (gameResult == 'win') {
+            result.textContent = `Player ${sign.toUpperCase()} won!`;
+            cells.forEach(cell => cell.style.pointerEvents = "none");
+        } else if (gameResult == 'tie') {
+            result.textContent = `It's a tie!`;
+        } 
+        
+        player.switchSign();
+    }
+
+    cells.forEach(cell => cell.addEventListener("click", _play));
+    startOverBtn.addEventListener('click', _startOver)
+})();
